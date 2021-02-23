8a9f2386e481df1f8fa724d5a8d4ac8afdaaa239
Reid Plumlee <reidjplumlee@gmail.com>
2021-02-23 15:07:23 -0800

    fix some styles, finish post

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `G-6FM5XKHW35`,
       },
     },
     {
